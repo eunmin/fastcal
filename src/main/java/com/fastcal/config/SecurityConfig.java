1730926b6e59e2cf599090fd2c7e5ccac96084e6
Eunmin Kim <telepopsound@gmail.com>
2026-01-01 21:47:55 +0900

    Use preferred_username

--- src/main/java/com/fastcal/config/SecurityConfig.java
+++ src/main/java/com/fastcal/config/SecurityConfig.java
@@ -194,7 +194,7 @@ public class SecurityConfig {
 
   private ReactiveJwtAuthenticationConverterAdapter jwtAuthenticationConverter() {
     JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
-    converter.setPrincipalClaimName("email");
+    converter.setPrincipalClaimName("preferred_username");
     return new ReactiveJwtAuthenticationConverterAdapter(converter);
   }
 
